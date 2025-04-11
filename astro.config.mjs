6da5df6aca1a8847e28b3f39b75259703440ff45
sorozco <sergiorozco.ochoa@gmail.com>
2025-04-11 18:57:00 -0500

    Changing adaptare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from '@astrojs/cloudflare';
-
 import db from '@astrojs/db';
+
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), db()],
   output: 'static',
-  adapter: cloudflare(),
+  adapter: vercel(),
 });
